c56300d168595c3a3cd2c805b2c4f0ac0ea9d69d
s2607 <swwiley@gmail.com>
2015-12-09 09:07:06 -0500

    oops

--- VB_Wiley-Stephen/Form2.vb
+++ VB_Wiley-Stephen/Form2.vb
@@ -1,5 +1,5 @@
 ﻿Public Class Form2
-    REM for next semester: use the file add command to make the new form.
+    REM for next semester: use the file add command to make the new form. (otherwise the namespace gets screwed up)
     REM I also could have started on this if you had drawn a UML diagram or described what was needed
     REM before you started building the UI on your machine
     Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
